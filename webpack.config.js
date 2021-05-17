9964e160f92f06d9a526aa3a6eaaa50daa7d1342
yongmin kim <kym930722@gmail.com>
2021-05-17 19:27:25 +0900

    remove publicPath

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,6 @@ module.exports = {
           {
             loader: 'file-loader',
             options: {
-              publicPath: './dist/',
               name: '[name].[ext]',
             },
           },
